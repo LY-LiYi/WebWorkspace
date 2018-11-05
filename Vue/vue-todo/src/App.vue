93450346b1d930f2097422985f4c5252fba4d912
LiYi <liyi0820@hotmail.com>
2018-11-06 00:23:23 +0800

    更新

---
+++ Vue/vue-todo/src/App.vue
@@ -0,0 +1,45 @@
+<template>
+  <div id="app">
+  	<div id="cover">
+  		<Header></Header>
+  		<Todo></Todo>
+  		<Footer></Footer>
+  	</div>
+  	
+  </div>
+</template>
+
+<script>
+import Header from '../src/todo/header.vue'
+import Footer from '../src/todo/footer.jsx'
+import Todo from '../src/todo/todo.vue'
+
+
+export default {
+  name: 'App',
+  components:
+  {
+  	Header,Footer,Todo,
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+#app{
+  position absolute
+  left 0
+  right 0
+  top 0
+  bottom 0
+}
+#cover{
+  position absolute
+  left 0
+  top 0
+  right 0
+  bottom 0
+  background-color #999
+  opacity .9
+  z-index -1
+}
+</style>
